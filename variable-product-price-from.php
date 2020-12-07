dfe46ef847cab41a0bf462ac38cd12407f07ccbc
Nathan Shanahan <nathan@nathanshanahan.com>
2020-12-07 11:43:27 +0800

    limit display to archive pages

--- variable-product-price-from.php
+++ variable-product-price-from.php
@@ -6,16 +6,23 @@
 add_filter('woocommerce_variable_price_html','ftf_shop_variable_product_price', 10, 2 );
 
 function ftf_shop_variable_product_price( $price, $product ){
+    
+    if(!is_single($product->get_id())) {
+        // Display From: Price on archive pages. 
+        $variation_min_reg_price = $product->get_variation_regular_price('min', true);
 
-    $variation_min_reg_price = $product->get_variation_regular_price('min', true);
+        if(!empty($variation_min_reg_price)) {
+          echo 'From: ' ; 
+            $price = woocommerce_price( $variation_min_reg_price );
+        }
+        else {
+            $price = woocommerce_price( $product->regular_price );
+        }
 
-    if(!empty($variation_min_reg_price)) {
-      echo 'From: ' ; 
-        $price = woocommerce_price( $variation_min_reg_price );
-    }
+        return $price;
+        
     else {
-        $price = woocommerce_price( $product->regular_price );
+        // display regular pricing on product single pages. 
+        return $price;
     }
-
-    return $price;
 }
