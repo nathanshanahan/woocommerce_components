8fb03687dc1ff748aec1028b30e765acf3cf1dda
Nathan Shanahan <nathan@nathanshanahan.com>
2021-02-01 14:49:55 +0800

    Create set-placeholder-text-checkout.php

---
+++ set-placeholder-text-checkout.php
@@ -0,0 +1,17 @@
+<?php 
+add_filter('woocommerce_checkout_fields', 'tft_set_placeholder_checkout_fields');
+function tft_set_placeholder_checkout_fields($fields)
+ {
+ unset($fields['billing']['billing_address_2']);
+  $fields['billing']['billing_company']['placeholder'] = 'Business Name';
+  $fields['billing']['billing_company']['label'] = 'Business Name';
+  $fields['billing']['billing_first_name']['placeholder'] = 'First Name'; 
+  $fields['shipping']['shipping_first_name']['placeholder'] = 'First Name';
+  $fields['shipping']['shipping_last_name']['placeholder'] = 'Surname';
+  $fields['shipping']['shipping_company']['placeholder'] = 'Company Name'; 
+  $fields['billing']['billing_last_name']['placeholder'] = 'Surname';
+  $fields['billing']['billing_email']['placeholder'] = 'Email Address ';
+  $fields['billing']['billing_phone']['placeholder'] = 'Phone ';
+  return $fields;
+ } 
+ ?>
